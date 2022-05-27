42b3e3ea7dc36555fd3e49e6bc5dee0b4b66ed7f
vidya163 <53250917+vidya163@users.noreply.github.com>
2022-05-27 21:16:12 +0530

    Update CreateContact.java

--- week2/day2.assignments/CreateContact.java
+++ week2/day2.assignments/CreateContact.java
@@ -8,32 +8,59 @@ public class CreateContact {
 		 * //Pseudo Code
 		 * 
 		 * 1. Launch URL "http://leaftaps.com/opentaps/control/login"
+		 System.setproperty(("webdriver.edge.driver", "C:\\edgedriver_win64\\msedgedriver.exe");
+		 WebDriver driver1 = new EdgeDriver();
+		 driver1.get(""http://leaftaps.com/opentaps/control/login");
+		 
 		 * 
 		 * 2. Enter UserName and Password Using Id Locator
+		 WebElement elename = driver1.FindElement(By.id("Username"));
+		 elename.sendKeys("Demosalesmanager");
+		 WebElement elename = driver1.FindElement(By.id("Password"));
+		 elename.sendkeys("crmsfa");
 		 * 
 		 * 3. Click on Login Button using Class Locator
+		 driver1.findElement(By.class("DecorativeSubmit")).click();
+		 
+		 
+		 
 		 * 
 		 * 4. Click on CRM/SFA Link
-		 * 
+		 * driver1.findElement(By.linkText("crmsfa")).click();
 		 * 5. Click on contacts Button
-		 * 
+		 * driver1.findElement(By.class("x-panel-header")).click();
 		 * 6. Click on Create Contact
-		 *  
+		 *  driver1.findElement(By.xpath("[//input@linkText = "Create Contact"]").click();
 		 * 7. Enter FirstName Field Using id Locator
-		 * 
+		 * WebElement elename = driver1.findElement(By.id("firstNameField"));
+		 elename.sendkeys("Vidhya");
 		 * 8. Enter LastName Field Using id Locator
+		 WebElement elename = driver1.findElement(By.id("lastNameField"));
+		 elename.sendkeys("Raghu");
 		 * 
 		 * 9. Enter FirstName(Local) Field Using id Locator
+		 WebElement elename = driver1.findElement(By.id("createContactForm_firstNameLocal"));
+		 elename.sendkeys("Vidya");
 		 * 
 		 * 10. Enter LastName(Local) Field Using id Locator
+		 WebElement elename = driver1.findElement(By.id("createContactForm_lastNameLocal"));
+		 elename.sendkeys("Rgahu");
 		 * 
 		 * 11. Enter Department Field Using any Locator of Your Choice
+		 WebElement elename = driver1.findElement(By.id("createContactForm_department"));
+		 elename.sendkeys("It");
 		 * 
 		 * 12. Enter Description Field Using any Locator of your choice 
+		 WebElement elename = driver1.findElement(By.id("createContactForm_description"));
+		 elename.sendkeys("Hi this is for testing");
+		 
 		 * 
 		 * 13. Enter your email in the E-mail address Field using the locator of your choice
+		 WebElement elename = driver1.findElement(By.id("createContactForm_primaryEmail"));
+		 elename.sendkeys("vidya5.rag@gmail.com");
 		 * 
 		 * 14. Select State/Province as NewYork Using Visible Text
+		 
 		 * 
 		 * 15. Click on Create Contact
 		 * 
